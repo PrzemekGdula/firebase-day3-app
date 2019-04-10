cb2116b563b587f0cdf7d027dc4846e77c7ad9e9
PrzemekGdula <przemo.g12@wp.pl>
2019-04-10 12:39:22 +0200

    Using on - only with callback - there is no promise there

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import { database } from './firebaseConf'
 
 // const courseRef = database.ref('/JFDDL7')
 
-database.ref('/JFDDL7').once(
-    'value'
-)
-    .then(snapshot => console.log(snapshot.val()))
+database.ref('/JFDDL7').on(
+    'value',
+    snapshot => console.log(snapshot.val()))
 
 const App = (props) => (
     <div>
